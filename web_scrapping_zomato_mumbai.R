282ab5183e32f2f80d095d73b271a3a48a68e285
akash1182 <56881790+akash1182@users.noreply.github.com>
2020-11-06 18:50:39 +0530

    Add files via upload

---
+++ web_scrapping_zomato_mumbai.R
@@ -0,0 +1,57 @@
+
+library(robotstxt)
+p = paths_allowed("https://www.zomato.com/pune/order-food-online?delivery_subzone=1165")
+
+#reading the website
+library(rvest)
+web = read_html("https://www.zomato.com/pune/order-food-online?delivery_subzone=1165")
+View(web)
+#importing dplyr library to work with dataframes
+library(dplyr)
+
+#reading names of restaurants
+name = web%>%html_nodes(".ln20")%>%html_text()
+View(name)
+
+#reading ratings
+ratings = web%>%html_nodes(".rating-value")%>%html_text()
+View(ratings)
+
+#reading cuisines
+cuisines = web%>%html_nodes(".clear+ .ln24")%>%html_text()
+View(cuisines)
+
+#reading average cost for 1 person
+cost_for_one = web%>%html_nodes(".ln24+ .grey-text")%>%html_text()
+View(cost_for_one)
+
+#reading minimum cost and delivery time
+min_cost_delivery_time = web%>%html_nodes(".ln24~ .grey-text+ .ln24")%>%html_text()
+View(min_cost_delivery_time)
+
+#reading payment methods
+payment_methods = web%>%html_nodes(".ln24:nth-child(5)")%>%html_text()
+View(payment_methods)
+
+#reading total number of reviews
+total_reviews = web%>%html_nodes("#orig-search-list .medium")%>%html_text()
+View(total_reviews)
+
+#creating a dataframe and concatinating the above lists
+zomato_mumbai_df = data.frame(name, cuisines, ratings, total_reviews,cost_for_one, min_cost_delivery_time, payment_methods)
+View(zomato_mumbai_df)
+
+#analyze
+dim(zomato_mumbai_df)
+str(zomato_mumbai_df)
+
+zomato_mumbai_df$cost_for_one = gsub("\\Cost ???|\\ for one","",zomato_mumbai_df$cost_for_one)
+View(zomato_mumbai_df)
+
+zomato_mumbai_df$total_reviews = gsub("\\(|\\)","",zomato_mumbai_df$total_reviews)
+zomato_mumbai_df$total_reviews = gsub("\\.|\\ Reviews","",zomato_mumbai_df$total_reviews)
+zomato_mumbai_df$total_reviews = gsub("K","000",zomato_mumbai_df$total_reviews)
+zomato_mumbai_df$total_reviews = gsub(",","",zomato_mumbai_df$total_reviews)
+
+zomato_mumbai_df$payment_methods = gsub("\\Accepts |\\ payments only","",zomato_mumbai_df$payment_methods)
+zomato_mumbai_df$payment_methods = gsub("payments","",zomato_mumbai_df$payment_methods)
